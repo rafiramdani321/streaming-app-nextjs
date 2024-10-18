5ce6378b6e9f0060417bb2626dd6200dc0153eff
rafiramdani321 <rafiramdani321@gmail.com>
2024-10-18 17:32:11 +0700

    update webhook livekit

--- app/api/webhooks/livekit/route.ts
+++ app/api/webhooks/livekit/route.ts
@@ -9,35 +9,48 @@ const receiver = new WebhookReceiver(
 );
 
 export async function POST(req: Request) {
-  const body = await req.text();
-  const headerPayload = headers();
-  const authorization = headerPayload.get("Authorization");
-
-  if(!authorization){
-    return new Response("No authorization header", { status: 400 });
-  }
-
-  const event = receiver.receive(body, authorization);
-
-  if(event.event === "ingress_started"){
-    await db.stream.update({
-      where: {
-        ingressId: event.ingressInfo?.ingressId
-      },
-      data: {
-        isLive: true
-      },
-    });
-  }
-
-  if(event.event === "ingress_ended"){
-    await db.stream.update({
-      where: {
-        ingressId: event.ingressInfo?.ingressId
-      },
-      data: {
-        isLive: false
-      },
-    });
+  try {
+    const body = await req.text();
+    const headerPayload = headers();
+    const authorization = headerPayload.get("Authorization");
+  
+    if(!authorization){
+      return new Response("No authorization header", { status: 400 });
+    }
+  
+    const event = receiver.receive(body, authorization);
+  
+    console.log(event.event)
+  
+    if(event.event === "ingress_started"){
+      await db.stream.update({
+        where: {
+          ingressId: event.ingressInfo?.ingressId
+        },
+        data: {
+          isLive: true
+        },
+      });
+  
+      return new Response("Ingress started event processed", { status: 200 });
+    }
+  
+    if(event.event === "ingress_ended"){
+      await db.stream.update({
+        where: {
+          ingressId: event.ingressInfo?.ingressId
+        },
+        data: {
+          isLive: false
+        },
+      });
+  
+      return new Response("Ingress ended event processed", { status: 200 });
+    }
+  
+    return new Response("Event not recognized", { status: 400 });
+  } catch (error) {
+    console.error("Error processing event: ", error);
+    return new Response("Internal server error", { status: 500 });
   }
 }
